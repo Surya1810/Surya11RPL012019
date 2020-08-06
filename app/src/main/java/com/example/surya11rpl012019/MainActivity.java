f73043ba04ae764887c05ca98d96c832075fdf6b
Surya1810 <surya.10rpl1.2019@gmail.com>
2020-08-06 18:58:43 +0700

    update + registration

--- app/src/main/java/com/example/surya11rpl012019/MainActivity.java
+++ app/src/main/java/com/example/surya11rpl012019/MainActivity.java
@@ -1,14 +1,17 @@
 package com.example.surya11rpl012019;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
-
-public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-}
+import android.os.Handler;
+import android.util.Pair;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
